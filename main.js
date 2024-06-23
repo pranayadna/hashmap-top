f225d92e813fb3ddbb79d822c2cce90d8f28b7f8
pranayadna <pranayadnaap96@gmail.com>
2024-06-23 15:50:00 +0700

    Add remove method

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function createHashMap() {
         return hashCode;
     }
 
-    const set = (key, value) => {    
+    const set = (key, value) => {
         const hashCode = hash(key);
-        
+
         const pair = {
             [key]: value,
             next: null
@@ -27,34 +27,34 @@ function createHashMap() {
                 const keyCheckBucket = Object.keys(buckets[i]);
                 const keyBucket = keyCheckBucket[0];
                 const hashKeyBucket = hash(keyBucket);
-    
+
                 if (key in buckets[i]) {
                     buckets[i][key] = value;
                 } else if (hashKeyBucket === hashCode) {
                     // Update key value inside linked list
                     const head = buckets[i];
                     let tmp = head;
-    
+
                     while (tmp !== null) {
                         const keyCheckTmp = Object.keys(tmp);
                         const keyTmp = keyCheckTmp[0];
-    
+
                         if (keyTmp === key) {
                             tmp[key] = value;
                             break;
-                        } 
+                        }
 
-                        if (tmp.next === null) {       
+                        if (tmp.next === null) {
                             tmp.next = pair;
                             break;
                         }
-    
+
                         tmp = tmp.next;
                     }
                 }
             } else if (hashCode === i) {
                 buckets[i] = pair;
-            }  
+            }
         }
     }
 
@@ -63,15 +63,15 @@ function createHashMap() {
             if (buckets[i] !== undefined) {
                 const keyCheckBucket = Object.keys(buckets[i]);
                 const keyBucket = keyCheckBucket[0];
-                
+
                 // console.log(keyBucket);
                 // console.log(buckets[i].hasOwnProperty(key));
-            //     if (key === keyBucket) {
-                    
-            //     }
+                //     if (key === keyBucket) {
+
+                //     }
                 if (buckets[i].hasOwnProperty(key)) {
                     return buckets[i][key];
-                } 
+                }
                 else {
                     const head = buckets[i];
                     let tmp = head;
@@ -79,7 +79,7 @@ function createHashMap() {
                     while (tmp !== null) {
                         const keyCheckTmp = Object.keys(tmp);
                         const keyTmp = keyCheckTmp[0];
-    
+
                         // console.log(tmp);
                         // console.log(tmp.hasOwnProperty(key));
 
@@ -88,18 +88,18 @@ function createHashMap() {
                         //     break;
                         // } 
 
-                        if (tmp.hasOwnProperty(key)) {       
+                        if (tmp.hasOwnProperty(key)) {
                             return tmp;
                         }
-    
+
                         tmp = tmp.next;
                     }
                 }
-            } 
+            }
             // else 
-            
+
         }
-        
+
         return null;
     }
 
@@ -108,15 +108,15 @@ function createHashMap() {
             if (buckets[i] !== undefined) {
                 const keyCheckBucket = Object.keys(buckets[i]);
                 const keyBucket = keyCheckBucket[0];
-                
+
                 // console.log(keyBucket);
                 // console.log(buckets[i].hasOwnProperty(key));
-            //     if (key === keyBucket) {
-                    
-            //     }
+                //     if (key === keyBucket) {
+
+                //     }
                 if (buckets[i].hasOwnProperty(key)) {
                     return true;
-                } 
+                }
                 else {
                     const head = buckets[i];
                     let tmp = head;
@@ -124,7 +124,7 @@ function createHashMap() {
                     while (tmp !== null) {
                         const keyCheckTmp = Object.keys(tmp);
                         const keyTmp = keyCheckTmp[0];
-    
+
                         // console.log(tmp);
                         // console.log(tmp.hasOwnProperty(key));
 
@@ -133,22 +133,47 @@ function createHashMap() {
                         //     break;
                         // } 
 
-                        if (tmp.hasOwnProperty(key)) {       
+                        if (tmp.hasOwnProperty(key)) {
                             return true;
                         }
-    
+
                         tmp = tmp.next;
                     }
                 }
-            } 
+            }
             // else 
-            
+
         }
-        
+
         return false;
     }
 
     const remove = (key) => {
+        const hashCode = hash(key);
+        console.log(hashCode);
+
+        if (buckets[hashCode] !== undefined) {
+            const head = buckets[hashCode];
+            let tmp = head;
+            let prev = null;
+
+            while (tmp !== null) {
+                // console.log(tmp);
+                for (const key in tmp) {
+                    // const key = 'next'
+                    console.log(tmp[key]);
+                    // console.log(key);
+                    // if (key === 'next') {
+
+                    // }
+
+                }
+
+                prev = tmp;
+                tmp = tmp.next;
+            }
+        }
+
         for (let i = 0; i < buckets.length; i++) {
             if (buckets[i] !== undefined) {
                 const nextCheckBucket = Object.values(buckets[i]);
@@ -160,7 +185,7 @@ function createHashMap() {
                 // console.log(nextCheckBucket);
                 // console.log(keyBucket);
                 // console.log(entriesCheckBucket);
-                
+
                 // buckets[i].hasOwnProperty(key) && 
                 if (nextBucket !== null) {
                     // const keyCheckBucket = Object.keys(buckets[i].next);
@@ -168,7 +193,7 @@ function createHashMap() {
                     // console.log(keyCheckBucket);
 
                     if (key in buckets[i]) {
-                        
+
                     }
                     // console.log(buckets[i]);
                     // buckets.splice(i, 1);
@@ -181,52 +206,52 @@ function createHashMap() {
                     let prev = null;
 
                     // console.log(head);
-                    while (current !== null ) {
+                    while (current !== null) {
                         const keyCheckCurrent = Object.keys(current);
                         const keyCurrent = keyCheckCurrent[0];
-    
+
                         // console.log(current.next);
                         // console.log(current);
                         // console.log(current.hasOwnProperty(key));
-    
+
                         if (keyCurrent === key) {
                             // current[key] = value;
-                            prev = current;   
+                            prev = current;
                             prev.next = current.next;
                             // console.log(prev);
                             // break;
-                        } 
-    
+                        }
+
                         // if (current.hasOwnProperty(key)) {    
                         // }
-                            
+
                         current = current.next;
                     }
 
-                        // console.log(prev.next);
+                    // console.log(prev.next);
                     // console.log(prev.next);
                     // return true;
-                } 
+                }
                 // else {
-                    
-                    
-    
-                    
-                // }
-                
 
-            // //     const keyCheckBucket = Object.keys(buckets[i]);
-            // //     const keyBucket = keyCheckBucket[0];
-                
-            // //     // console.log(keyBucket);
-            // //     // console.log(buckets[i].hasOwnProperty(key));
-            // // //     if (key === keyBucket) {
-                    
-            // // //     }
-            // //     else {
+
+
+
+                // }
+
+
+                // //     const keyCheckBucket = Object.keys(buckets[i]);
+                // //     const keyBucket = keyCheckBucket[0];
+
+                // //     // console.log(keyBucket);
+                // //     // console.log(buckets[i].hasOwnProperty(key));
+                // // //     if (key === keyBucket) {
+
+                // // //     }
+                // //     else {
 
             }
-        } 
+        }
     }
 
     return {
@@ -254,9 +279,9 @@ hashMap.set('Oppenheimer', 'Atom reaction');
 
 console.log(buckets);
 
+console.log(hashMap.remove('Door'));
 // console.log(hashMap.remove('Bump'));
-console.log(hashMap.remove('Bump'));
 // console.log(hashMap.remove('Carlos'));
 // console.log(hashMap.get('rafa'));
 // console.log(hashMap.has('Carlos'));
-console.log(buckets);
+// console.log(buckets);
